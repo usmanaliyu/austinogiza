8f37c50cf040fa5d2722d2021632b8412e11f8da
usmanaliyu <53077750+usmanaliyu@users.noreply.github.com>
2020-04-08 00:42:04 +0200

    Update signup.py

--- signup.py
+++ signup.py
@@ -5,12 +5,49 @@ import os
 import sys
 import sqlite3
 
-user = []
+
+def connect():
+    conn = sqlite3.connect('database.db')
+    conn.commit()
+    conn.close()
+
+
+def table():
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    cur.execute('CREATE TABLE IF NOT EXISTS user(id INTEGER PRIMARY KEY, first_name VARCHAR(100), last_name VARCHAR(100), user VARCHAR(100), email VARCHAR(50), password VARCHAR(100) )')
+    conn.close()
+    conn.close()
+
+def adduser(first_name, last_name, email, password, user):
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    cur.execute("INSERT INTO user VALUES(Null, ?, ?, ?, ?, ?)",(first_name, last_name, user,email, password))
+    conn.commit()
+    conn.close()
+
+def allusers():
+    conn = sqlite3.connect('database.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM user")
+    row = cur.fetchall()
+    conn.commit()
+    conn.close()
+    return row
+
+connect()
+table()
+print( 'Avalable users:')
+print(allusers())
+
+
+
 first = input("What is your first name?: ")
 last = input("What is your last name?: ")
 email = input("Enter your email: ")
-name = first + last
-user.append(name)
+user_name = first + last
+
+
 
 
 def randomString():
@@ -29,14 +66,16 @@ if first and last != "":
     answer = input("Are you satisfied with the password")
 if answer == 'no':
     newpass = input("input desired password: ")
+    while len(newpass) < 7:
+        print('Enter a password more than 7 letters')
+        newpass = input("input desired password: ")
+
     print(newpass)
+
+    adduser(first_name=first, last_name=last, user=user_name, email=email, password=newpass)
+
 elif answer == "yes":
     print("Your password is: " + str(password))
-if len(newpass) < 7:
-    print('Enter a password more than 7 letters')
-    newpass = input("input desired password: ")
+    adduser(first_name=first, last_name=last, user=user_name, email= email, password=password)
 
 
-pickle.dump(user, open("user.dat", "wb"))
-
-user = pickle.load(open("user.dat", "rb"))
